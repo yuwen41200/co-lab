51eb0ce34c92b8058b37356b09de1f626c8cc435
yuwen41200 <yuwen41200@gmail.com>
2016-03-13 04:09:43 +0800

    Fix module arch.

--- lab01/alu.v
+++ lab01/alu.v
@@ -3,21 +3,21 @@
  * by NCTU CS 0000000 Po-han Chen, 0316213 Yu-wen Pwu
  */
 
-module alu(
+module alu (
 	input         rst_n,         // negative reset
 	input  [31:0] src1,          // source 1
 	input  [31:0] src2,          // source 2
 	input  [3:0]  ALU_control,   // ALU control input
 	input  [2:0]  bonus_control, // bonus control input
 	output [31:0] result,        // result
-	output        zero,          // when the output is 0, zero must be set
+	output        zero,          // must be set when the output is 0
 	output        cout,          // carry out
 	output        overflow       // overflow
 );
 
 wire [31:0] less;
-wire        A_invert;
-wire        B_invert;
+wire        src1_invert;
+wire        src2_invert;
 wire [32:0] cin;
 wire [1:0]  operation;
 wire [31:0] real_result;
@@ -32,9 +32,9 @@ wire [31:0] real_result;
 // SLT  0111 -> bonus_control
 
 assign less[31:1] = 31'b0;
-assign A_invert = (ALU_control == 4'b1100 || ALU_control == 4'b1101) ? 1 : 0;
-assign B_invert = (ALU_control == 4'b0110 || ALU_control == 4'b1100 ||
-                   ALU_control == 4'b1101 || ALU_control == 4'b0111) ? 1 : 0;
+assign src1_invert = (ALU_control == 4'b1100 || ALU_control == 4'b1101) ? 1 : 0;
+assign src2_invert = (ALU_control == 4'b0110 || ALU_control == 4'b1100 ||
+                      ALU_control == 4'b1101 || ALU_control == 4'b0111) ? 1 : 0;
 assign cin[0] = (ALU_control == 4'b0110 || ALU_control == 4'b0111) ? 1 : 0;
 assign operation = ((ALU_control == 4'b0000 || ALU_control == 4'b1100) ? 2'b00 // &
                  : ((ALU_control == 4'b0001 || ALU_control == 4'b1101) ? 2'b01 // |
@@ -62,13 +62,13 @@ assign less[0] = ((bonus_control == 3'b000 &&   real_result[31] == 1)           
 
 generate
 	genvar i;
-	for (i = 0; i < 32; i = i + 1) begin:alu_top_group
-		alu_top alu_top_ins (
+	for (i = 0; i < 32; i = i + 1) begin:alu_single_group
+		alu_single alu_single_ins (
 			.src1(src1[i]),
 			.src2(src2[i]),
 			.less(less[i]),
-			.A_invert(A_invert),
-			.B_invert(B_invert),
+			.src1_invert(src1_invert),
+			.src2_invert(src2_invert),
 			.cin(cin[i]),
 			.operation(operation),
 			.real_result(real_result[i]),
--- lab01/alu_top.v
+++ lab01/alu_top.v
@@ -1,52 +1,25 @@
-`timescale 1ns/1ps
+/**
+ * A Single Bit Arithmetic Logic Unit
+ * by NCTU CS 0000000 Po-han Chen, 0316213 Yu-wen Pwu
+ */
 
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    10:58:01 02/25/2016
-// Design Name: 
-// Module Name:    alu_top 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
-
-module alu_top(
-               src1,       //1 bit source 1 (input)
-               src2,       //1 bit source 2 (input)
-               less,       //1 bit less     (input)
-               A_invert,   //1 bit A_invert (input)
-               B_invert,   //1 bit B_invert (input)
-               cin,        //1 bit carry in (input)
-               operation,  //operation      (input)
-               result,     //1 bit result   (output)
-               cout,       //1 bit carry out(output)
-               );
-
-input         src1;
-input         src2;
-input         less;
-input         A_invert;
-input         B_invert;
-input         cin;
-input [2-1:0] operation;
-
-output        result;
-output        cout;
+module alu_single (
+	input        src1,
+	input        src2,
+	input        less,
+	input        src1_invert,
+	input        src2_invert,
+	input        cin,
+	input  [1:0] operation,
+	output       real_result,
+	output       result,
+	output       cout
+);
 
 wire A, B;
 
-assign A = src1 ^ A_invert;
-assign B = src2 ^ B_invert;
+assign A = src1 ^ src1_invert;
+assign B = src2 ^ src2_invert;
 
 assign result = operation == 2'b00 ? (A & B)
 	: (operation == 2'b01 ? (A | B)
