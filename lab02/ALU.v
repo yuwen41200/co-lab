004d079680677d52bf999c70c27196238592a725
Jasmine Chen <j.lin013@gmail.com>
2016-04-24 01:51:29 +0800

    fix indentation

--- lab02/ALU.v
+++ lab02/ALU.v
@@ -11,18 +11,18 @@
 
 module ALU(
     src1_i,
-	src2_i,
-	ctrl_i,
-	result_o,
-	zero_o
-	);
+    src2_i,
+    ctrl_i,
+    result_o,
+    zero_o
+    );
      
 //I/O ports
 input  [32-1:0]  src1_i;
-input  [32-1:0]	 src2_i;
+input  [32-1:0]  src2_i;
 input  [4-1:0]   ctrl_i;
 
-output [32-1:0]	 result_o;
+output [32-1:0]  result_o;
 output           zero_o;
 
 //Internal signals
@@ -35,22 +35,17 @@ assign zero_o = (result_o == 0);
 
 //Main function
 always @(*) begin
-	case (ctrl_i)
-		0: result_o <= src1_i & src2_i;
-		1: result_o <= src1_i | src2_i;
-		2: result_o <= src1_i + src2_i;
-		6: result_o <= src1_i - src2_i;
-		7: result_o <= src1_i < src2_i ? 1 : 0;
-		12: result_o <= ~(src1_i | src2_i);
-		default: result_o <= 0;
-	endcase
+    case (ctrl_i)
+        0: result_o <= src1_i & src2_i;
+        1: result_o <= src1_i | src2_i;
+        2: result_o <= src1_i + src2_i;
+        6: result_o <= src1_i - src2_i;
+        7: result_o <= src1_i < src2_i ? 1 : 0;
+        12: result_o <= ~(src1_i | src2_i);
+        default: result_o <= 0;
+    endcase
 end
 
 endmodule
 
 
-
-
-
-                    
-                    
--- lab02/Decoder.v
+++ lab02/Decoder.v
@@ -11,12 +11,12 @@
 
 module Decoder(
     instr_op_i,
-	RegWrite_o,
-	ALU_op_o,
-	ALUSrc_o,
-	RegDst_o,
-	Branch_o
-	);
+    RegWrite_o,
+    ALU_op_o,
+    ALUSrc_o,
+    RegDst_o,
+    Branch_o
+    );
      
 //I/O ports
 input  [6-1:0] instr_op_i;
@@ -39,15 +39,15 @@ reg            Branch_o;
 
 //Main function
 always @(*) begin
-	RegWrite_o <= (instr_op_i == 0 || instr_op_i == 35); // R-type or Load -> nead to write result to some register
-	ALU_op_o   <= (instr_op_i == 35 || instr_op_i == 43) ? 2 : // Load or Store -> add
-	              (instr_op_i == 4 ? 6 : // Branch -> sub
-					  (instr_op_i == 8 ? 2 : // ADDI -> add
-					  (instr_op_i == 9 ? 6 : // SLTIU -> sub
-					  4'b1111))); // Else -> check with funct in instruction
-	ALUSrc_o   <= (instr_op_i == 35 || instr_op_i == 43); // Load or Store -> multiplexer selects address as the second ALU source
-	RegDst_o   <= (instr_op_i == 0); // R-type -> multiplexer selects [15:11] as the register to write to
-	Branch_o   <= (instr_op_i == 4); // Branch
+    RegWrite_o <= (instr_op_i == 0 || instr_op_i == 35); // R-type or Load -> nead to write result to some register
+    ALU_op_o   <= (instr_op_i == 35 || instr_op_i == 43) ? 2 : // Load or Store -> add
+                  (instr_op_i == 4 ? 6 : // Branch -> sub
+                  (instr_op_i == 8 ? 2 : // ADDI -> add
+                  (instr_op_i == 9 ? 6 : // SLTIU -> sub
+                  4'b1111))); // Else -> check with funct in instruction
+    ALUSrc_o   <= (instr_op_i == 35 || instr_op_i == 43); // Load or Store -> multiplexer selects address as the second ALU source
+    RegDst_o   <= (instr_op_i == 0); // R-type -> multiplexer selects [15:11] as the register to write to
+    Branch_o   <= (instr_op_i == 4); // Branch
 end
 
 endmodule
