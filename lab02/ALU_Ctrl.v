95691fefccf1f40b4283b082f8d2bb1a0befaee6
Jasmine Chen <j.lin013@gmail.com>
2016-04-24 01:47:26 +0800

    finish components, pending interconnection

---
+++ lab02/ALU_Ctrl.v
@@ -0,0 +1,53 @@
+//Subject:     CO project 2 - ALU Controller
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module ALU_Ctrl(
+          funct_i,
+          ALUOp_i,
+          ALUCtrl_o
+          );
+          
+//I/O ports 
+input      [6-1:0] funct_i;
+input      [4-1:0] ALUOp_i;
+
+output     [4-1:0] ALUCtrl_o;    
+     
+//Internal Signals
+reg        [4-1:0] ALUCtrl_o;
+
+//Parameter
+
+       
+//Select exact operation
+always @(*) begin
+	if (ALUOp_i != 4'b1111)
+		ALUCtrl_o <= ALUOp_i;
+	else begin
+		case (funct_i)
+			32: ALUCtrl_o <= 2; // ADD
+			34: ALUCtrl_o <= 6; // SUB
+			36: ALUCtrl_o <= 0; // AND
+			37: ALUCtrl_o <= 1; // OR
+			42: ALUCtrl_o <= 4; // SLT
+			default: ALUCtrl_o <= 4'b1111;
+		endcase
+	end
+end
+
+endmodule     
+
+
+
+
+
+                    
+                    
---
+++ lab02/MUX_2to1.v
@@ -0,0 +1,36 @@
+//Subject:     CO project 2 - MUX 221
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+     
+module MUX_2to1(
+               data0_i,
+               data1_i,
+               select_i,
+               data_o
+               );
+
+parameter size = 32;			   
+			
+//I/O ports               
+input   [size-1:0] data0_i;          
+input   [size-1:0] data1_i;
+input              select_i;
+output  [size-1:0] data_o; 
+
+//Internal Signals
+// reg     [size-1:0] data_o;
+
+assign data_o = select_i == 1 ? data0_i : data1_i;
+
+//Main function
+
+endmodule      
+          
+          
