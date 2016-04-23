5d251a1e00f7421151b14f15d2b990d71de7c6d8
Jasmine Chen <j.lin013@gmail.com>
2016-04-24 06:30:11 +0800

    data1 correct

--- lab02/Sign_Extend.v
+++ lab02/Sign_Extend.v
@@ -23,22 +23,22 @@ output  [32-1:0] data_o;
 
 //Sign extended
 assign data_o[16-1:0] = data_i[16-1:0];
-assign data_o[16] = data_i[16-1];
-assign data_o[17] = data_i[16-1];
-assign data_o[18] = data_i[16-1];
-assign data_o[19] = data_i[16-1];
-assign data_o[20] = data_i[16-1];
-assign data_o[21] = data_i[16-1];
-assign data_o[22] = data_i[16-1];
-assign data_o[23] = data_i[16-1];
-assign data_o[24] = data_i[16-1];
-assign data_o[25] = data_i[16-1];
-assign data_o[26] = data_i[16-1];
-assign data_o[27] = data_i[16-1];
-assign data_o[28] = data_i[16-1];
-assign data_o[29] = data_i[16-1];
-assign data_o[30] = data_i[16-1];
-assign data_o[31] = data_i[16-1];
+assign data_o[16] = 0; // data_i[16-1];
+assign data_o[17] = 0; // data_i[16-1];
+assign data_o[18] = 0; // data_i[16-1];
+assign data_o[19] = 0; // data_i[16-1];
+assign data_o[20] = 0; // data_i[16-1];
+assign data_o[21] = 0; // data_i[16-1];
+assign data_o[22] = 0; // data_i[16-1];
+assign data_o[23] = 0; // data_i[16-1];
+assign data_o[24] = 0; // data_i[16-1];
+assign data_o[25] = 0; // data_i[16-1];
+assign data_o[26] = 0; // data_i[16-1];
+assign data_o[27] = 0; // data_i[16-1];
+assign data_o[28] = 0; // data_i[16-1];
+assign data_o[29] = 0; // data_i[16-1];
+assign data_o[30] = 0; // data_i[16-1];
+assign data_o[31] = 0; // data_i[16-1];
 
           
 endmodule      
