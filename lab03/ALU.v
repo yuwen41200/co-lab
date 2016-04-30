6531670d951fa43e6f077f4024cb4ed2952e59b6
Jasmine Chen <j.lin013@gmail.com>
2016-04-30 06:04:13 +0100

    Initiate lab03 project skeleton

---
+++ lab03/ALU.v
@@ -0,0 +1,58 @@
+//Subject:     CO project 2 - ALU
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module ALU(
+    src1_i,
+    src2_i,
+    ctrl_i,
+    result_o,
+    zero_o
+    );
+     
+//I/O ports
+input [32-1:0]       src1_i;
+input [32-1:0]       src2_i;
+input [4-1:0]        ctrl_i;
+
+output [32-1:0]      result_o;
+output               zero_o;
+
+//Internal signals
+wire signed [32-1:0] src1_i_signed;
+wire signed [32-1:0] src2_i_signed;
+reg [32-1:0]         result_o;
+wire                 zero_o;
+
+assign src1_i_signed = src1_i;
+assign src2_i_signed = src2_i;
+
+assign zero_o = (result_o == 0);
+
+//Parameter
+
+//Main function
+always @(*) begin
+    case (ctrl_i)
+        0: result_o = src1_i & src2_i;
+        1: result_o = src1_i | src2_i;
+        2: result_o = src1_i + src2_i;
+        6: result_o = src1_i - src2_i;
+        7: result_o = src1_i < src2_i ? 1 : 0;
+        12: result_o = ~(src1_i | src2_i);
+        14: result_o = src2_i_signed >>> src1_i_signed;
+        15: result_o = src2_i << 16;
+        default: result_o = 0;
+    endcase
+end
+
+endmodule
+
+
---
+++ lab03/Decoder.v
@@ -0,0 +1,62 @@
+//Subject:     CO project 2 - Decoder
+//--------------------------------------------------------------------------------
+//Version:     1
+//--------------------------------------------------------------------------------
+//Writer:      
+//----------------------------------------------
+//Date:        
+//----------------------------------------------
+//Description: 
+//--------------------------------------------------------------------------------
+
+module Decoder(
+    instr_op_i,
+    RegWrite_o,
+    ALU_op_o,
+    ALUSrc_o,
+    RegDst_o,
+    Branch_o
+    );
+     
+//I/O ports
+input  [6-1:0] instr_op_i;
+
+output         RegWrite_o;
+output [4-1:0] ALU_op_o;
+output         ALUSrc_o;
+output         RegDst_o;
+output         Branch_o;
+ 
+//Internal Signals
+reg    [4-1:0] ALU_op_o;
+reg            ALUSrc_o;
+reg            RegWrite_o;
+reg            RegDst_o;
+reg            Branch_o;
+
+//Parameter
+
+
+//Main function
+always @(*) begin
+    RegWrite_o = (instr_op_i == 0 || instr_op_i == 35 || instr_op_i == 8 || instr_op_i == 9 || instr_op_i == 15 || instr_op_i == 13); // R-type, Load, ADDI, SLTIU, LUI, ORI -> nead to write result to some register
+    ALU_op_o   = (instr_op_i == 35 || instr_op_i == 43) ? 2 : // Load or Store -> add
+                 (instr_op_i == 4 ? 6 : // Branch -> sub
+                 (instr_op_i == 8 ? 2 : // ADDI -> add
+                 (instr_op_i == 9 ? 7 : // SLTIU -> lt
+                 (instr_op_i == 13 ? 1 : // ORI -> or
+                 (instr_op_i == 15 ? 15: // LUI -> Left shift immediate by 16
+                 4'b1111))))); // Else -> check with funct in instruction
+    ALUSrc_o   = (instr_op_i == 35 || instr_op_i == 43 || instr_op_i == 8 || instr_op_i == 9 || instr_op_i == 15 || instr_op_i == 13); // Load, Store, ADDI, SLTIU, LUI, ORI -> multiplexer selects [15:0] as the second ALU source
+    RegDst_o   = (instr_op_i == 0); // R-type -> multiplexer selects [15:11] as the register to write to
+    Branch_o   = (instr_op_i == 4 || instr_op_i == 5); // BEQ, BNE
+end
+
+endmodule
+
+
+
+
+
+                    
+                    
