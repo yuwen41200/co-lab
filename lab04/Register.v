1d8c7a492f8fa86871c90f65f835c967d94124e6
yuwen41200 <yuwen41200@gmail.com>
2016-05-30 03:45:10 +0800

    Bug fixed; all basic test data are correct.

--- lab04/Register.v
+++ lab04/Register.v
@@ -17,22 +17,22 @@ assign rt_data_o = reg_file[rt_addr_i];
 
 always @(posedge clk_i) begin
 	if (~rst_i) begin
-		reg_file[0]  <= 0; reg_file[1]  <= 0;   reg_file[2]  <= 0; reg_file[3]  <= 0;
-		reg_file[4]  <= 0; reg_file[5]  <= 0;   reg_file[6]  <= 0; reg_file[7]  <= 0;
-		reg_file[8]  <= 0; reg_file[9]  <= 0;   reg_file[10] <= 0; reg_file[11] <= 0;
-		reg_file[12] <= 0; reg_file[13] <= 0;   reg_file[14] <= 0; reg_file[15] <= 0;
-		reg_file[16] <= 0; reg_file[17] <= 0;   reg_file[18] <= 0; reg_file[19] <= 0;
-		reg_file[20] <= 0; reg_file[21] <= 0;   reg_file[22] <= 0; reg_file[23] <= 0;
-		reg_file[24] <= 0; reg_file[25] <= 0;   reg_file[26] <= 0; reg_file[27] <= 0;
-		reg_file[28] <= 0; reg_file[29] <= 124; reg_file[30] <= 0; reg_file[31] <= 0;
-		// Register 29 is Stack Pointer. Initiate it to the top.
+		reg_file[0]  <= 0; reg_file[1]  <= 0; reg_file[2]  <= 0; reg_file[3]  <= 0;
+		reg_file[4]  <= 0; reg_file[5]  <= 0; reg_file[6]  <= 0; reg_file[7]  <= 0;
+		reg_file[8]  <= 0; reg_file[9]  <= 0; reg_file[10] <= 0; reg_file[11] <= 0;
+		reg_file[12] <= 0; reg_file[13] <= 0; reg_file[14] <= 0; reg_file[15] <= 0;
+		reg_file[16] <= 0; reg_file[17] <= 0; reg_file[18] <= 0; reg_file[19] <= 0;
+		reg_file[20] <= 0; reg_file[21] <= 0; reg_file[22] <= 0; reg_file[23] <= 0;
+		reg_file[24] <= 0; reg_file[25] <= 0; reg_file[26] <= 0; reg_file[27] <= 0;
+		reg_file[28] <= 0; reg_file[29] <= 0; reg_file[30] <= 0; reg_file[31] <= 0;
 	end
-	else begin
-		if (reg_write_i)
-			reg_file[rd_addr_i] <= rd_data_i;
-		else
-			reg_file[rd_addr_i] <= reg_file[rd_addr_i];
-	end
+end
+
+always @(*) begin
+	if (reg_write_i)
+		reg_file[rd_addr_i] <= rd_data_i;
+	else
+		reg_file[rd_addr_i] <= reg_file[rd_addr_i];
 end
 
 endmodule
--- lab04/Testbench.v
+++ lab04/Testbench.v
@@ -25,11 +25,11 @@ end
 always @(posedge clk) begin
 	count = count + 1;
 	if (count == 30) begin
-		$display("\nRegister===========================================================\n");
-		$display("r0=%d, r1=%d, r2=%d, r3=%d, r4=%d, r5=%d, r6=%d, r7=%d\n",
+		$display("Register===========================================================\n");
+		$display(" r0=%d,  r1=%d,  r2=%d,  r3=%d,  r4=%d,  r5=%d,  r6=%d,  r7=%d\n",
 		Cpu.Register.reg_file[0],  Cpu.Register.reg_file[1],  Cpu.Register.reg_file[2],  Cpu.Register.reg_file[3],
 		Cpu.Register.reg_file[4],  Cpu.Register.reg_file[5],  Cpu.Register.reg_file[6],  Cpu.Register.reg_file[7]);
-		$display("r8=%d, r9=%d, r10=%d, r11=%d, r12=%d, r13=%d, r14=%d, r15=%d\n",
+		$display(" r8=%d,  r9=%d, r10=%d, r11=%d, r12=%d, r13=%d, r14=%d, r15=%d\n",
 		Cpu.Register.reg_file[8],  Cpu.Register.reg_file[9],  Cpu.Register.reg_file[10], Cpu.Register.reg_file[11],
 		Cpu.Register.reg_file[12], Cpu.Register.reg_file[13], Cpu.Register.reg_file[14], Cpu.Register.reg_file[15]);
 		$display("r16=%d, r17=%d, r18=%d, r19=%d, r20=%d, r21=%d, r22=%d, r23=%d\n",
@@ -39,11 +39,11 @@ always @(posedge clk) begin
 		Cpu.Register.reg_file[24], Cpu.Register.reg_file[25], Cpu.Register.reg_file[26], Cpu.Register.reg_file[27],
 		Cpu.Register.reg_file[28], Cpu.Register.reg_file[29], Cpu.Register.reg_file[30], Cpu.Register.reg_file[31]);
 
-		$display("\nMemory=============================================================\n");
-		$display("m0=%d, m1=%d, m2=%d, m3=%d, m4=%d, m5=%d, m6=%d, m7=%d\n",
+		$display("Memory=============================================================\n");
+		$display(" m0=%d,  m1=%d,  m2=%d,  m3=%d,  m4=%d,  m5=%d,  m6=%d,  m7=%d\n",
 		Cpu.DataMemory.mem_debug[0],  Cpu.DataMemory.mem_debug[1],  Cpu.DataMemory.mem_debug[2],  Cpu.DataMemory.mem_debug[3],
 		Cpu.DataMemory.mem_debug[4],  Cpu.DataMemory.mem_debug[5],  Cpu.DataMemory.mem_debug[6],  Cpu.DataMemory.mem_debug[7]);
-		$display("m8=%d, m9=%d, m10=%d, m11=%d, m12=%d, m13=%d, m14=%d, m15=%d\n",
+		$display(" m8=%d,  m9=%d, m10=%d, m11=%d, m12=%d, m13=%d, m14=%d, m15=%d\n",
 		Cpu.DataMemory.mem_debug[8],  Cpu.DataMemory.mem_debug[9],  Cpu.DataMemory.mem_debug[10], Cpu.DataMemory.mem_debug[11],
 		Cpu.DataMemory.mem_debug[12], Cpu.DataMemory.mem_debug[13], Cpu.DataMemory.mem_debug[14], Cpu.DataMemory.mem_debug[15]);
 		$display("r16=%d, m17=%d, m18=%d, m19=%d, m20=%d, m21=%d, m22=%d, m23=%d\n",
